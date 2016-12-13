5818e4dfe0ce41bbaa7a391a00988189a73fc2a1
Christoph Fiehe <christoph.fiehe@materna.de>
2016-12-13 10:01:18 +0100

    Mountpoint resource added to avoid remounts on refresh events when share gets mounted.

--- manifests/fs.pp
+++ manifests/fs.pp
@@ -16,13 +16,15 @@
 class ceph::fs (
   $package_names  = ['ceph-fs-common', 'ceph-fuse'],
   $package_ensure = 'installed',
-  $mounts         = {}) {
+  $mounts         = {},
+  $mountpoints    = {}) {
 
   include ::ceph
 
   package { $package_names: ensure => $package_ensure, }
 
   create_resources('::ceph::fs::mount', $mounts)
+  create_resources('::ceph::fs::mountpoint', $mountpoints)
 
   Class['::ceph'] -> Class['::ceph::fs']
 }
